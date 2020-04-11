4c0a2df762198a8f5fda3da6b2b24c0555a0dbcb
AbdelliNasredine <abdou.nasredine@gmail.com>
2020-04-11 04:23:32 +0100

    :-|

--- utmb-redesign/script.js
+++ utmb-redesign/script.js
@@ -7,6 +7,7 @@
 
   window.onscroll = function (event) {
     var scroll_position = this.scrollY;
+
     if (scroll_position >= jumbotron.offsetHeight)
       navbar.classList.add("fixed");
     else navbar.classList.remove("fixed");
